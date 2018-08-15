a8e5274d7f6c5e1e7bd07e927ceddaf89a966fef
Juan Antonio Cano <jacanosalado@gmail.com>
2018-08-16 00:57:28 +0200

    v3 added

---
+++ src/yolov3/yolotest/ImageUtilHelper.cs
@@ -0,0 +1,44 @@
+﻿using OpenCvSharp;
+using System;
+
+namespace Tensorflow
+{
+    public static class ImageUtilHelper
+    {
+        public static void AddBoxToImage(Mat image, string text, float left, float top, float right, float bottom, Scalar color, int fontSizeHeight)
+        {
+            var xP1 = Math.Min(Math.Max(left, 0), image.Width - 1);
+            var yP1 = Math.Min(Math.Max(top, 0), image.Height - 1);
+
+            var xP2 = Math.Min(Math.Max(right, 0), image.Width - 1);
+            var yP2 = Math.Min(Math.Max(bottom, 0), image.Height - 1);
+
+            var pt1 = new Point(xP1, yP1);
+            var pt2 = new Point(xP2, yP2);
+
+            Cv2.Rectangle(image, pt1, pt2, color);
+
+            var org = new Point(left, top - fontSizeHeight);
+            Cv2.PutText(image, text, org, HersheyFonts.HersheyPlain, 1.0, color);
+        }
+
+        public static Mat CropImage(Mat image, float left, float top, float right, float bottom)
+        {
+            var x = left;
+            var y = top;
+
+            var width = (right - left);
+            var height = (bottom - top);
+
+            var imgRect = new Rect(new Point(0, 0), new Size(image.Width, image.Height));
+            var rectCrop = new Rect(new Point(x, y), new Size(width, height));
+
+            var final = rectCrop.Intersect(imgRect);
+
+            var finalImage = new Mat(image, final);
+            var croppedImage = finalImage.Clone();
+
+            return croppedImage;
+        }
+    }
+}
---
+++ src/yolov3/yolotest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Darknet;
+using OpenCvSharp;
+using Tensorflow;
+
+namespace yolotest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var yolo = new YoloWrapper("yolov3.cfg", "yolov3.weights", 0);
+
+            var windowCapture = new Window("windowCapture");
+            var capture = new VideoCapture(0);
+            var image = new Mat();
+
+             while (true)
+            {
+                capture.Read(image);
+                if (image.Empty())
+                    break;
+
+                var bytes = image.ToBytes(".png");
+                var bbox = yolo.Detect(bytes);
+                if (bbox == null)
+                {
+                    continue;
+                }
+
+                var items = bbox.ToArray();
+
+                foreach (var item in items)
+                {
+                    var value = (item.prob * 100).ToString("0");
+                    var text = $"{item.obj_id} - {value}%";
+
+                    var error = item.x > item.y;
+                    if (error)
+                    {
+                        continue;
+                    }
+
+                    ImageUtilHelper.AddBoxToImage(image, text, (float)item.x, (float)item.y, (float)item.y + item.w, (float)item.y + item.h, new Scalar(255,0,0), 10);
+                }
+
+                windowCapture.ShowImage(image);
+
+                if ((Cv2.WaitKey(25) & 0xFF) == 'q')
+                {
+                    Cv2.DestroyAllWindows();
+                    break;
+                }
+            }
+        }
+    }
+}
